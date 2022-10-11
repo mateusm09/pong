3c06bbc2f1e524a21823148a36cc2f9d37d25108
Mateus <developer@mindtech.com.br>
2022-10-10 22:49:16 -0300

    bouncing ball test

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,15 @@
 import Phaser from 'phaser';
 
-export default {
+export const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
     parent: 'game',
     backgroundColor: '#33A5E7',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+        },
+    },
     scale: {
         width: 800,
         height: 600,
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -5,20 +5,35 @@ export default class Demo extends Phaser.Scene {
         super('GameScene');
     }
 
-    preload() {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-    }
+    preload() {}
 
     create() {
-        const logo = this.add.image(400, 70, 'logo');
+        const upper = this.add.rectangle(400, 100, 600, 10, 0xffffff);
+        const lower = this.add.rectangle(400, 500, 600, 10, 0xffffff);
+        const left = this.add.rectangle(100, 300, 10, 400, 0xffffff);
+        const right = this.add.rectangle(700, 300, 10, 400, 0xffffff);
 
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1,
-        });
+        const boundaries = this.physics.add.staticGroup([upper, lower, left, right]);
+
+        const ball = this.add.circle(400, 300, 10, 0xffffff);
+        const ballPhysics = this.physics.add.group(ball);
+        ballPhysics.setVelocity(100, 100);
+
+        this.physics.add.collider(ballPhysics, boundaries, this.onHitBorders, undefined, this);
+    }
+
+    onHitBorders(
+        ball: Phaser.Types.Physics.Arcade.GameObjectWithBody,
+        border: Phaser.Types.Physics.Arcade.GameObjectWithBody,
+    ) {
+        console.log('collision detected');
+
+        if (ball.body.blocked.right || ball.body.blocked.left) {
+            ball.body.velocity.x *= -1;
+        }
+
+        if (ball.body.blocked.up || ball.body.blocked.down) {
+            ball.body.velocity.y *= -1;
+        }
     }
 }
